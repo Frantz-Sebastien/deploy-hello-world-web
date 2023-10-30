f57cd40962379dd4b8c2047b3a6cf1c341c53fd4
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-10-30 18:18:58 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let newElement = document.createElement("p");
+
+// Set the text content for this paragraph
+newElement.textContent = "Hello, World!";
+
+// Append the paragraph to the body of the document
+document.body.appendChild(newElement);
